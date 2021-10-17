9e4e947866c089fec45d2a9e3af40d22eb9fcb99
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-17 06:25:59 +0000

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e670226212496117b2f16993e6e2c90e4f51050e28dfc9ff2aefb9f412c635225ef60fe1669af6956461b2061d3791ee2e0d326e837fe37fe19b5db979c64c47'
